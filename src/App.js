bf152c6c7c8dba2d3f3ff731e312c929bd04716f
Mati Gotta <mati.gotta@pm.me>
2023-02-10 20:56:26 -0300

    Ha

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>HOLAAAAAAAAAAAA-S</h1>
+    </div>
+  );
+}
+
+export default App;
